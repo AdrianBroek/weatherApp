2031d5b225b4d7b3bb1acc3ecddaf9661faee975
Adrianbro96@gmail.com <Adrianbro96@gmail.com>
2022-09-27 22:22:03 +0200

    backround color to linear

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -8,8 +8,10 @@ const GlobalStyles = createGlobalStyle`
         padding: 0;
     }
     body {
-        background-color: #0e203c;
-        /* color: RGB(225, 93, 68); */
+        /* background-color: #0e203c; */
+        background: #2c3e50;  /* fallback for old browsers */
+        background: -webkit-linear-gradient(120deg, #2b83bd, #2c3e50);  /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(120deg, #2b83bd, #2c3e50); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
         color: #fff;
         background-size: cover;
         background-repeat: no-repeat;
@@ -17,12 +19,13 @@ const GlobalStyles = createGlobalStyle`
         background-origin: content-box;
         -webkit-transition: background-image 0.2s ease-in-out;
         transition: background-image 0.2s ease-in-out;
+        height: 100%;
     }
     .map {
         height: 200px;
         width: 100%;
         z-index: -1;
-        filter: brightness(0.5);
+        filter: brightness(0.9);
         border-radius: 0.5rem;
         overflow: hidden;
         margin: 3rem 0;
